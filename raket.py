0b9597fcd93d38c2160df6bf5acb34f37adac49c
Otto Hammar <otto.hammar.4350@student.uu.se>
2023-09-14 12:21:05 +0200

    final_final

--- raket.py
+++ raket.py
@@ -70,14 +70,14 @@ def find_best_theta():
     print("best theta:",best_theta)
     return best_theta
             
-goal = (70,60)
 
+goal = (80,60)
 c = 0.05
 k = 700
+h = 1.5
 
 t_0 = 0.0 # starttid
-t_1 = 45.0 # sluttid
-h = 0.1
+t_1 = 30.0 # sluttid
 t_span = (t_0, t_1)
 t_range = np.arange(t_0,t_1,h)
 u_0 = [0,0,0,0] #x, y, vx, vy
@@ -89,44 +89,75 @@ rk_solution = runge_kutta(f,t_range,u_0, (c,k,theta_m))
 
 ivp_solution = scipy.integrate.solve_ivp(f,t_span, u_0, method="RK45", args=(c,k,theta_m), max_step = h)
 
-#starta upp animation
-fig, axes = plt.subplots()
-axes.set(xlim=[-100, 700], ylim=[0, 800])
+def animate_solution():
+    #starta upp animation
+    fig, axes = plt.subplots()
+    axes.set(xlim=[-100, 700], ylim=[0, 800])
 
-#plotta rk_solution
-rk_tail = axes.plot((0,0) ,"r", linestyle = "-")
-rk_artist = axes.plot(0, 0, "b.", label="Vår Runge-Kutta")
+    #plotta rk_solution
+    rk_tail = axes.plot((0,0) ,"r", linestyle = "-")
+    rk_artist = axes.plot(0, 0, "b.", label="Vår Runge-Kutta")
 
-#plotta ivp_solution
-ivp_tail = axes.plot((0,0) ,"b", linestyle = "-")
-ivp_artist = axes.plot(0, 0, "g.", label="Inbyggd lösare")
+    #plotta ivp_solution
+    ivp_tail = axes.plot((0,0) ,"b", linestyle = "-")
+    ivp_artist = axes.plot(0, 0, "g.", label="Inbyggd lösare")
 
-artists = [rk_artist, ivp_artist]
-tails = [rk_tail, ivp_tail]
+    artists = [rk_artist, ivp_artist]
+    tails = [rk_tail, ivp_tail]
 
-goal = axes.plot(goal[0], goal[1], "rx", label="Mål")
+    goal_artist = axes.plot(goal[0], goal[1], "rx", label="Mål")
 
-axes.legend()
+    axes.legend()
 
-#update funktionen definierar hur animationen ser ut
-def update(frame):
-    #vår lösares positioner och solve_ivps positioner har olika "shape"
-    rk_position = (rk_solution[frame][0], rk_solution[frame][1])
-    ivp_position = (ivp_solution.y[0][frame], ivp_solution.y[1][frame])
+    #update funktionen definierar hur animationen ser ut
+    def update(frame):
+        #vår lösares positioner och solve_ivps positioner har olika "shape"
+        rk_position = (rk_solution[frame][0], rk_solution[frame][1])
+        ivp_position = (ivp_solution.y[0][frame], ivp_solution.y[1][frame])
 
-    positions = [rk_position, ivp_position]
+        positions = [rk_position, ivp_position]
+        
+        #animerar båda raketerna
+        for artist, tail, pos  in zip(artists, tails, positions):
+            px, py = pos
+
+            artist[0].set_data((px, py))
+
+            theta = -(np.pi / 2) if py < 20 else theta_m
+
+            rx, ry = (px + 10 *np.cos(theta), py + 10 * np.sin(theta))
+            tail[0].set_data([(px,rx),(py,ry)])
+
+    ani = animation.FuncAnimation(fig = fig, func = update, interval = 1, frames = range(len(t_range)))
+    ani.save(f"{goal} h={h}.gif", writer="ImageMagickWriter")
+    # plt.show()
+
+def plot_solution():
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2)
+    fig.set_size_inches(9,9)
+    fig.suptitle(f'Goal vid {goal}, h={h}', fontsize=16)
+
+    ax1.set_title("Inbyggd lösare position")
+    ax2.set_title("Inbyggd lösare hastighet")
     
-    #animerar båda raketerna
-    for artist, tail, pos  in zip(artists, tails, positions):
-        px, py = pos
+    ax1.plot(ivp_solution.t, ivp_solution.y[0], label="x")
+    ax1.plot(ivp_solution.t, ivp_solution.y[1], label="y")
+    ax1.legend()
+    
+    ax2.plot(ivp_solution.t, ivp_solution.y[2], label="x velocity")
+    ax2.plot(ivp_solution.t, ivp_solution.y[3], label="y velocity")
+    ax2.legend()
 
-        artist[0].set_data(px, py)
+    ax3.set_title("Vår lösare position")
+    ax4.set_title("Vår lösare hastighet")
 
-        theta = -(np.pi / 2) if py < 20 else theta_m
+    rk_plot = np.transpose(rk_solution)
+    ax3.plot(t_range, rk_plot[0], label="x")
+    ax3.plot(t_range, rk_plot[1], label="y")
+    ax3.legend()
+    ax4.plot(t_range, rk_plot[2], label="x velocity")
+    ax4.plot(t_range, rk_plot[3], label="y velocity")
+    ax4.legend()
+    plt.show()
 
-        rx, ry = (px + 10 *np.cos(theta), py + 10 * np.sin(theta))
-        tail[0].set_data([(px,rx),(py,ry)])
-
-ani = animation.FuncAnimation(fig = fig, func = update, interval = 1, frames = range(len(t_range)))
-
-plt.show()
+animate_solution()
